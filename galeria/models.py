22675f826cbb43a0f23c26e487bb14aa596f6d7d
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2023-12-26 00:00:39 -0300

    Corrigindo  nome do campo

--- galeria/models.py
+++ galeria/models.py
@@ -16,7 +16,7 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     publicada = models.BooleanField(default=False)
-    data_foografia = models.DateTimeField(default=datetime.now, blank=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
