274a9d4cc8ae5cc9e76a32c5cd5c18867c668cad
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2023-12-27 23:53:03 -0300

    Alterando formulário de cadastro

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -24,3 +24,50 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Pedro Silva"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label="Email",
+        required=False,
+        max_length=100,
+        widget=forms.EmailInput(
+             attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: pedrosilva@xpto.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
