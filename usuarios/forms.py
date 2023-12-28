7a3fab3d693417b62a06984a3aa614eccb6134e2
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2023-12-27 23:35:32 -0300

    Estilizando formulário

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,11 +5,22 @@ class LoginForms(forms.Form):
     nome_login = forms.CharField(
         label="Nome de Login",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Pedro Silva"
+            }
+        )
     )
     senha = forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
     )
